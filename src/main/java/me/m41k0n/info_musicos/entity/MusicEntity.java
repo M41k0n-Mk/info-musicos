464559e4ed19e21a38a8625a0cbd56bf89bbac44
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2024-11-30 18:00:21 -0300

    feat: implemented search for songs by artist. refact imports.

--- src/main/java/me/m41k0n/info_musicos/entity/MusicEntity.java
+++ src/main/java/me/m41k0n/info_musicos/entity/MusicEntity.java
@@ -1,6 +1,12 @@
 package me.m41k0n.info_musicos.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
