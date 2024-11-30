b1644ec2a9ca4c8834b99cb108256bc79ddb7fe9
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2024-11-30 03:25:22 -0300

    feat: Implemented music listing

--- src/main/java/me/m41k0n/info_musicos/actions/ListMusicAction.java
+++ src/main/java/me/m41k0n/info_musicos/actions/ListMusicAction.java
@@ -1,7 +1,18 @@
 package me.m41k0n.info_musicos.actions;
 
+import me.m41k0n.info_musicos.model.Music;
+import me.m41k0n.info_musicos.repository.MusicRepository;
+
 public class ListMusicAction implements MenuAction {
+    private final MusicRepository musicRepository;
+
+    public ListMusicAction(MusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
     @Override
     public void execute() {
+        musicRepository.findAll().stream().map(m -> new Music(m.getName(), m.getAlbum(), m.getBand()))
+                .forEach(System.out::println);
     }
 }
